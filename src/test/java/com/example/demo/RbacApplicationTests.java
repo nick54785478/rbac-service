4d07cffba4ae040ab51a05214fe383c93383e4aa
nick54785478 <40881607+nick54785478@users.noreply.github.com>
2025-01-15 02:39:57 +0800

    調整 class Name

--- src/test/java/com/example/demo/RbacApplicationTests.java
+++ src/test/java/com/example/demo/RbacApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RabcDemoApplicationTests {
+class RbacApplicationTests {
 
 	@Test
 	void contextLoads() {
