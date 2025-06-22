aeb229e4c3bea15312984c8492832b05c94f4227
HONEY\nick5 <genghao2021@gmail.com>
2025-06-22 23:02:13 +0800

    移除註解

--- src/main/java/com/example/demo/service/UserQueryService.java
+++ src/main/java/com/example/demo/service/UserQueryService.java
@@ -1,14 +1,10 @@
 package com.example.demo.service;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.service.UserService;
-import com.example.demo.domain.share.UserGroupQueried;
 import com.example.demo.domain.share.UserInfoDetailQueried;
 import com.example.demo.domain.share.UserInfoQueried;
-import com.example.demo.domain.share.UserRoleQueried;
 import com.example.demo.domain.user.aggregate.UserInfo;
 import com.example.demo.infra.repository.UserInfoRepository;
 import com.example.demo.util.BaseDataTransformer;
@@ -24,30 +20,6 @@ public class UserQueryService {
 
 	private UserService userService;
 	private UserInfoRepository userRepository;
-
-//	/**
-//	 * 取得特定使用者所在的 Group 資料
-//	 * 
-//	 * @param username 使用者帳號
-//	 * @return List<UserGroupQueried> 使用者群組清單
-//	 */
-//	public List<UserGroupQueried> queryGroups(String username) {
-//		List<UserGroupQueried> groups = userService.queryGroups(username);
-//		return groups;
-//	}
-//
-//	/**
-//	 * 取得特定使用者所在的 Role 資料
-//	 * 
-//	 * @param username 使用者帳號
-//	 * @param service  服務
-//	 * @return List<UserRoleQueried>
-//	 */
-//	public List<UserRoleQueried> queryRoles(String username, String service) {
-//		List<UserRoleQueried> roles = userService.queryRoles(username);
-//		log.debug("roles: {}", roles);
-//		return roles;
-//	}
 
 	/**
 	 * 查詢該使用者資訊
