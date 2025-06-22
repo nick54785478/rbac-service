865f07ec236b1e1a44e2210f56379d99008a5b9f
HONEY\nick5 <genghao2021@gmail.com>
2025-06-23 00:11:55 +0800

    移除已刪除的DTO

--- src/main/java/com/example/demo/service/UserCommandService.java
+++ src/main/java/com/example/demo/service/UserCommandService.java
@@ -8,7 +8,6 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.domain.service.UserService;
-import com.example.demo.domain.share.UserRolesGranted;
 import com.example.demo.domain.user.aggregate.UserInfo;
 import com.example.demo.domain.user.command.CreateUserCommand;
 import com.example.demo.domain.user.command.UpdateUserCommand;
