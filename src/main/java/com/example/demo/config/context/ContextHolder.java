cc09edeb14b4637659aeed637999a5d4d83b4e69
HONEY\nick5 <genghao2021@gmail.com>
2025-08-24 18:48:38 +0800

    調整 ContextHolder.

--- src/main/java/com/example/demo/config/context/ContextHolder.java
+++ src/main/java/com/example/demo/config/context/ContextHolder.java
@@ -6,12 +6,15 @@ import java.util.List;
 import com.example.demo.config.security.JwtConstants;
 
 import io.jsonwebtoken.Claims;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 /**
  * 上下文工具類
  */
 @Slf4j
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ContextHolder {
 
 	/**
