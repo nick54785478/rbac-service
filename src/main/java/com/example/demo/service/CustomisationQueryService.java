60e133fd9839cd3cad5f6eccaaf640e7aa9e316e
HONEY\nick5 <genghao2021@gmail.com>
2025-12-19 17:17:22 +0800

    調整個人化配置

--- src/main/java/com/example/demo/service/CustomisationQueryService.java
+++ src/main/java/com/example/demo/service/CustomisationQueryService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.customisation.aggregate.Customisation;
@@ -30,9 +31,25 @@ public class CustomisationQueryService {
 	public List<FieldViewCustomisationQueried> getCustomisation(String username, String component, String type) {
 		Customisation customisation = customisationRepository
 				.findByUsernameAndComponentAndType(username, component, type).orElse(null);
-		if (Objects.isNull(customisation)) {
+		if (!this.checkValue(customisation)) {
 			return new ArrayList<>();
 		}
 		return JsonParseUtil.unserializeArrayOfObject(customisation.getValue(), FieldViewCustomisationQueried.class);
+	}
+
+	/**
+	 * 檢查個人化配置資料
+	 * 
+	 * @param customisation 個人化配置
+	 * @return 是否合乎規則
+	 */
+	private boolean checkValue(Customisation customisation) {
+		if (Objects.isNull(customisation)) {
+			return false;
+		}
+		if (StringUtils.isBlank(customisation.getValue())) {
+			return false;
+		}
+		return true;
 	}
 }
